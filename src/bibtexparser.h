34da3f8832ef1863defd4520fa31aeb1d66abfc4
IHateYourCode <89636625+IHateYourCode@users.noreply.github.com>
2021-10-19 06:29:43 -0400

    Bibtex formatting

--- src/bibtexparser.h
+++ src/bibtexparser.h
@@ -3,19 +3,28 @@
 
 #include "mostQtHeaders.h"
 
-class BibTeXFileInfo
-{
-public:
-	//these are the properties of the file when it was loaded the last time (no watching)
-	QTextCodec *codec;
-	QDateTime lastModified;
-	QSet<QString> ids;
-	QString linksTo;
-	bool loadIfModified(const QFileInfo &fi);
-private:
-	void load(const QFileInfo &fi);
-protected: //unit tests access this
-	void parse(QByteArray &data);
+
+using Info = const QFileInfo &;
+
+
+class BibTeXFileInfo {
+
+	public:
+		//these are the properties of the file when it was loaded the last time (no watching)
+		QTextCodec * codec;
+		QDateTime lastModified;
+		QSet<QString> ids;
+		QString linksTo;
+
+		bool loadIfModified(Info info);
+
+	private:
+
+		void load(Info info);
+
+	protected: //unit tests access this
+
+		void parse(QByteArray & data);
 };
 
-#endif // BIBTEXPARSER_H
+#endif
