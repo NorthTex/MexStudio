26049b8ac260fc29af1d92ff128f97bed549985c
IHateYourCode <89636625+IHateYourCode@users.noreply.github.com>
2021-10-19 07:00:05 -0400

    LatexRepository formatting

--- src/latexrepository.cpp
+++ src/latexrepository.cpp
@@ -1,58 +1,81 @@
+
 #include "latexrepository.h"
 #include <QMutex>
 
-LatexRepository *LatexRepository::m_Instance = nullptr;
 
-LatexRepository::LatexRepository() :
-    QObject(nullptr), m_dataSource(None)
-{
+using Source = LatexRepository::DataSource;
+
+
+LatexRepository * LatexRepository::m_Instance = nullptr;
+
+
+LatexRepository::LatexRepository()
+    : QObject(nullptr)
+    , m_dataSource(None){
+
 	loadStaticPackageList(":/utilities/packageList");
 }
 
-LatexRepository *LatexRepository::instance()
-{
+
+LatexRepository * LatexRepository::instance(){
+
 	static QMutex mutex;
 	mutex.lock();
-	if (!m_Instance)
+
+	if(!m_Instance)
 		m_Instance = new LatexRepository();
+
 	mutex.unlock();
+
 	return m_Instance;
 }
 
-LatexRepository::DataSource LatexRepository::dataSource()
-{
+
+Source LatexRepository::dataSource(){
 	return m_dataSource;
 }
 
-bool LatexRepository::loadStaticPackageList(const QString &file)
-{
-	if (file.isEmpty()) return false;
+
+bool LatexRepository::loadStaticPackageList(const QString & path){
+
+	if(path.isEmpty())
+		return false;
+
 	packages.reserve(3000);
 
-	QFile f(file);
-	if (! f.open(QFile::ReadOnly)) return false;
+	QFile file(path);
 
-	while (!f.atEnd()) {
-		QString line = f.readLine().trimmed();
-		if (line.startsWith('#')) continue;
+	if(!file.open(QFile::ReadOnly))
+		return false;
+
+	while(!file.atEnd()){
+
+		QString line = file.readLine().trimmed();
+
+		if(line.startsWith('#'))
+			continue;
+
 		int sep = line.indexOf(':');
-		if (sep < 0) {
+
+		if(sep < 0){
 			packages.insert(line, LatexPackageInfo(line));
 		} else {
 			QString name = line.left(sep);
-			packages.insert(name, LatexPackageInfo(name, line.mid(sep + 1)));
+			packages.insert(name, LatexPackageInfo(name,line.mid(sep + 1)));
 		}
 	}
+
 	m_dataSource = Static;
+
 	return true;
 }
 
-bool LatexRepository::packageExists(const QString &name)
-{
+
+bool LatexRepository::packageExists(const QString & name){
     return packages.contains(name);
 }
 
-QString LatexRepository::shortDescription(const QString &name)
-{
+
+QString LatexRepository::shortDescription(const QString & name){
 	return packages[name].shortDescription;
 }
