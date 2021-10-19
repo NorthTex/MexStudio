c4bfb4ea9cb008ec73137f42a9f5e9d27018d070
IHateYourCode <89636625+IHateYourCode@users.noreply.github.com>
2021-10-19 04:17:34 -0400

    UserMacro.h formatting

--- src/usermacro.h
+++ src/usermacro.h
@@ -6,90 +6,118 @@
 #include <QJsonDocument>
 #include <QJsonArray>
 
+
 class QLanguageDefinition;
 class QLanguageFactory;
-//class LatexDocument;
 
 
-class Macro
-{
-public:
-	enum SpecialTrigger { ST_NO_TRIGGER = 0,
-	                      ST_REGEX = 1, ST_TXS_START = 2,
-	                      ST_NEW_FILE = 4, ST_NEW_FROM_TEMPLATE = 8, ST_LOAD_FILE = 0x10, ST_LOAD_THIS_FILE = 0x20,
-	                      ST_FILE_SAVED = 0x40, ST_FILE_CLOSED = 0x80, ST_MASTER_CHANGED = 0x100,
-	                      ST_AFTER_TYPESET = 0x200, ST_AFTER_COMMAND_RUN = 0x400
-	                    };
-	enum Type { Snippet, Environment, Script };
+class Macro {
 
-	Q_DECLARE_FLAGS(SpecialTriggers, SpecialTrigger)
-	Macro();
-	Macro(const QString &nname, const Macro::Type ntype, const QString &ntag, const QString &nabbrev, const QString &ntrigger);
-	Macro(const QString &nname, const QString &typedTag, const QString &nabbrev=QString(), const QString &ntrigger=QString());
-	Macro(const QStringList &fieldList);
+	public:
 
-	static Macro fromTypedTag(const QString &typedTag);
+		enum Type { Snippet, Environment, Script };
 
-    QString name, abbrev,description,menu;
-	Type type;
-	QString trigger;
-	QRegExp triggerRegex;
-	bool triggerLookBehind;
+		enum SpecialTrigger {
+			ST_NO_TRIGGER = 0,
+			ST_REGEX = 1,
+			ST_TXS_START = 2,
+			ST_NEW_FILE = 4,
+			ST_NEW_FROM_TEMPLATE = 8,
+			ST_LOAD_FILE = 0x10,
+			ST_LOAD_THIS_FILE = 0x20,
+			ST_FILE_SAVED = 0x40,
+			ST_FILE_CLOSED = 0x80,
+			ST_MASTER_CHANGED = 0x100,
+			ST_AFTER_TYPESET = 0x200,
+			ST_AFTER_COMMAND_RUN = 0x400
+		};
 
-	QStringList toStringList() const;
+		Q_DECLARE_FLAGS(SpecialTriggers, SpecialTrigger)
 
-	QString snippet() const;
-	QString script() const;
-    QString shortcut() const;
+		Macro(const QString & nname,const QString & typedTag,const QString & nabbrev = QString(),const QString & ntrigger = QString());
+		Macro(const QString & nname,const Macro::Type ntype,const QString & ntag,const QString & nabbrev,const QString & ntrigger);
+		Macro(const QStringList & fieldList);
+		Macro();
 
-    bool isEmpty() const;
+		static Macro fromTypedTag(const QString & typedTag);
 
-    void setShortcut(const QString &sc);
-    void setTrigger(const QString &newTrigger);
 
-	QString typedTag() const;
-    void setTypedTag(const QString &m_tag);
-	static QString parseTypedTag(QString typedTag, Macro::Type &retType);
+		QString name, abbrev , description , menu , trigger;
+		QRegExp triggerRegex;
+		Type type;
 
-	void parseTriggerLanguage(QLanguageFactory *langFactory);
-	bool isActiveForTrigger(SpecialTrigger trigger) const;
-	bool isActiveForLanguage(QLanguageDefinition *lang) const;
-	bool isActiveForFormat(int format) const;
+		bool triggerLookBehind;
 
-    bool load(const QString &fileName);
-    bool loadFromText(const QString &text);
-    bool save(const QString &fileName) const;
 
-	LatexDocument *document;
+		QStringList toStringList() const;
 
-private:
-	void init(const QString &nname, Macro::Type ntype, const QString &ntag, const QString &nabbrev, const QString &ntrigger);
-	void initTriggerFormats();
+		QString shortcut() const;
+		QString snippet() const;
+		QString script() const;
 
-	QString tag;
-	SpecialTriggers triggers;
+		bool isEmpty() const;
 
-	QString triggerLanguage;
-	QList<QLanguageDefinition *> triggerLanguages;
+		void setShortcut(const QString & shortcut);
+		void setTypedTag(const QString & typedTag);
+		void setTrigger(const QString & trigger);
 
-	QString triggerFormatsUnprocessed;
-	QList<int> triggerFormats;
-	QString triggerFormatExcludesUnprocessed;
-	QList<int> triggerFormatExcludes;
+		QString typedTag() const;
 
-    QString m_shortcut;
+		static QString parseTypedTag(QString typedTag, Macro::Type &retType);
+
+		void parseTriggerLanguage(QLanguageFactory * langFactory);
+
+		bool isActiveForLanguage(QLanguageDefinition * lang) const;
+		bool isActiveForTrigger(SpecialTrigger trigger) const;
+		bool isActiveForFormat(int format) const;
+
+		bool loadFromText(const QString & text);
+		bool load(const QString & fileName);
+		bool save(const QString & fileName) const;
+
+		LatexDocument * document;
+
+	private:
+
+		void init(const QString &nname, Macro::Type ntype, const QString &ntag, const QString &nabbrev, const QString &ntrigger);
+		void initTriggerFormats();
+
+		QString tag;
+		QString triggerLanguage;
+		QString triggerFormatsUnprocessed;
+		QString triggerFormatExcludesUnprocessed;
+
+		SpecialTriggers triggers;
+
+		QList<QLanguageDefinition *> triggerLanguages;
+
+		QList<int> triggerFormats;
+		QList<int> triggerFormatExcludes;
+
+		QString m_shortcut;
 };
+
 
 Q_DECLARE_METATYPE(Macro);
 
-class MacroExecContext {
-public:
-	MacroExecContext() { triggerId = Macro::ST_NO_TRIGGER; }
-	MacroExecContext(int triggerType) { this->triggerId = triggerType; }
-	MacroExecContext(int triggerType, QStringList triggerMatches) { this->triggerId = triggerType; this->triggerMatches = triggerMatches; }
 
-	int triggerId;
-	QStringList triggerMatches;
+class MacroExecContext {
+
+	public:
+
+		int triggerId;
+		QStringList triggerMatches;
+
+
+		MacroExecContext()
+			: triggerId(Macro::ST_NO_TRIGGER) {};
+
+		MacroExecContext(int triggerType)
+			: triggerId(triggerType) {};
+
+		MacroExecContext(int triggerType,QStringList triggerMatches)
+			: triggerId(triggerType)
+			, triggerMatches(triggerMatches) {};
 };
 
-#endif // USERMACRO_H
+#endif
